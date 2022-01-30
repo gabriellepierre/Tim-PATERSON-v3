fc4cbf160f7fc6bf8a39128cc268cc1de1bbe685
Tiff <95696787+Tiffem@users.noreply.github.com>
2022-01-30 19:39:43 +0100

    19h39

--- src/Form/SearchForm.php
+++ src/Form/SearchForm.php
@@ -1,8 +1,30 @@
 <?php
 
+
 namespace App\Form;
 
-class SearchRecherche 
-{
+use App\Entity\Article;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
 
+class SearchForm extends AbstractType
+{
+    
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Article', EntityType::class, [
+            'class' => Article::class,
+            'choice_label' => 'title'
+        ]);
+        ;
+    }
 }
+ //tuto :https://www.youtube.com/watch?v=_75fDJITerA&ab_channel=SmaineMilianni
+/*->add('title', Article::class, [
+                'class' => title::class,
+                'choice_label' => 'name'
+                ]*/
+?>
